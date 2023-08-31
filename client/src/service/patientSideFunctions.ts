6dca8b576d6a618a3bb20858ceccfd8c0bc44483
Mujo78 <mujo.kalabuzic.20@size.ba>
2023-08-31 15:37:44 +0200

    commit -g

--- client/src/service/patientSideFunctions.ts
+++ client/src/service/patientSideFunctions.ts
@@ -1,32 +1,23 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3001/api/v1/appointment"
-const user = localStorage.getItem("user")
-const userObj = user && JSON.parse(user)
 
-/*
+export async function getPatientsForDoctor(token:string, id:string, page : number) {
 
-router.get("/doctor-patients/:id", restrictTo("DOCTOR"), getPatientsForDoctor)
-router.get("/search/:id", restrictTo("DOCTOR"), getPatientsForDoctorBySearch)
-
-*/
-
-export async function getPatientsForDoctor(page : number) {
-
-    const response = await axios.get(`${BASE_URL}/doctor-patients/${userObj.data._id}?page=${page}`, {
+    const response = await axios.get(`${BASE_URL}/doctor-patients/${id}?page=${page}`, {
         headers: {
-            "Authorization": `Bearer ${userObj.token}`
+            "Authorization": `Bearer ${token}`
         }
     })
 
     return response.data;
 }
 
-export async function searchForPatient(searchQuery: string, page: number) {
+export async function searchForPatient(token:string, id:string, searchQuery: string, page: number) {
 
-    const response = await axios.get(`${BASE_URL}/search/${userObj.data._id}?searchQuery=${searchQuery ||'none'}&page=${page || 'none'}`, {
+    const response = await axios.get(`${BASE_URL}/search/${id}?searchQuery=${searchQuery ||'none'}&page=${page || 'none'}`, {
         headers: {
-            "Authorization": `Bearer ${userObj.token}`
+            "Authorization": `Bearer ${token}`
         }
     })
 
